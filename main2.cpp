9ab74cceb0f4cc6bbb7060c0f7071b0130df6f8e
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-29 12:46:16 -0600

    ejercicio 2 del 29/05/2024

---
+++ main2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tabla[4][3];
+    for (int i = 0 <= 4; i++)
+    {
+        for ( int j = 0; j < 3; j++)
+        {
+            cout << "Ingrese el valor para la fila " << i+1 << ", columna " << j+1 << ": " << endl;
+            cin >> tabla[i][j];
+        }
+    }
+    int mayorsuma = 
+
+    return 0;
+}
