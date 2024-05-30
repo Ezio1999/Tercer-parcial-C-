e169c871d16fb6cf4d2345e51946ea7443510140
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-29 19:37:43 -0600

    ejercicio 1 del 29/05/2024  corregido

--- main.cpp
+++ main.cpp
@@ -2,19 +2,32 @@
 /*Escribir un programa */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int acum = 0, x, cont = 1;
-    for (int i = 0; i <= 20; i++)
+    int acum = 0;
+    vector<int> numbers;
+    for (int i = 1; i <= 20; i++)
     {
-        if (x > 0)
-        {
+        int x;
         cout << "Ingrese un numero " << i << ": " << endl;
         cin >> x;
         acum += x;
+        if (x == 0)
+        {
+            break;
         }
+
+        numbers.push_back(x);
+        acum += x;
+    }
+    cout << "El orden de los numeros inversos es: " << endl;
+    for (int i = numbers.size() - 1; i >= 0; i--)
+    {
+
+        cout << numbers[i] << endl;
     }
     cout << "La suma de los numeros es de: " << acum << endl;
 
