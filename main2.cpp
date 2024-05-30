14989b5d54f036a0d4c36c562dec819efe119d49
Castillo <=joseignacionavacastillo@gmail.com>
2024-05-29 20:01:38 -0600

    ejercicio 2 del 29/05/2024 corregido

--- main2.cpp
+++ main2.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int tabla[4][3];
-    for (int i = 0 <= 4; i++)
+    const int filas = 4;
+    const int columnas = 3;
+
+    vector<vector<int>> tabla(filas, vector<int>(columnas, 0));
+    vector<int> columnSums(columnas, 0);
+    for (int i = 0; i < filas; i++)
     {
-        for ( int j = 0; j < 3; j++)
+        for (int j = 0; j < columnas; j++)
         {
-            cout << "Ingrese el valor para la fila " << i+1 << ", columna " << j+1 << ": " << endl;
+            cout << "Ingrese el valor para la fila " << i + 1 << ", columna " << j + 1 << ": " << endl;
             cin >> tabla[i][j];
+
+            columnSums[j] += tabla[i][j];
         }
     }
-    int mayorsuma = 
+    int maxsum = columnSums[0];
+    int maxCol = 0;
+    for (int i = 1; i < columnas; i++)
+    {
+        if (columnSums[i] > maxsum)
+        {
+            maxsum = columnSums[i];
+            maxCol = i;
+        }
+    }
+    cout << "La mayor suma de columna es: " << maxsum << endl;
+    cout << "La columna con la mayor suma es la " << maxCol + 1 << endl;
 
     return 0;
 }
