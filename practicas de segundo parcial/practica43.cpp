40679d737e42739a5d06dc5b4e2f939db4d161b7
Castillo <=joseignacionavacastillo@gmail.com>
2024-06-03 16:49:40 -0600

    Mas ejercicios

---
+++ practicas de segundo parcial/practica43.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 100;
+    while (x==0)
+    {
+        cout << x-2 << endl;
+    }
+    return 0;
+}
---
+++ practicas de segundo parcial/practica48.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x , y , z=1 , i;
+    cout << "Ingrese un numero: " << endl;
+    cin >> x;
+    cout << "Ingrese el exponente: " << endl;
+    cin >> y;
+    for ( i = 0 ; i < y; i++)
+    {
+        z *= x;
+    }
+    cout << "EL resultado de la potencia es de: " << z << endl;
+
+    return 0;
+}
